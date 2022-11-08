b7cc3673d5e411b82bcfa6f2f7e423b0c75bff21
5Suminsss2 <sumin.kim.dev@gmail.com>
2022-11-08 15:37:58 +0900

    Design: Title 컴포넌트 디자인 구성

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,53 @@
+import { AiFillStar, AiFillBell } from "react-icons/ai";
+import { TbStar } from "react-icons/tb";
+import styled from "styled-components";
+
 function Title () {
-    return(
-        <div>
-            title
-        </div>
-    )
+
+    // CSS
+    const TitleContainer = styled.div`
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+    `;
+
+    const TitlePart = styled.div`
+        margin-left: 130px;
+        font-size: 20px;
+    `
+
+    const IconPart = styled.div`
+      display: flex;
+    `;
+    
+    const IconItem = styled.div`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-left: 5px;
+      width: 25px;
+      height: 25px;
+      border-radius: 50%;
+      border: 2px solid #d9d9d9;
+
+      &:hover {
+        background: #dee2e8;
+      }
+    `;
+
+    return (
+      <TitleContainer>
+        <TitlePart>Simple Wallet</TitlePart>
+        <IconPart>
+          <IconItem>
+            <AiFillStar size="15" color="#d9d9d9" />
+          </IconItem>
+          <IconItem>
+            <AiFillBell size="15" color="#d9d9d9" />
+          </IconItem>
+        </IconPart>
+      </TitleContainer>
+    );
 }
 
 export default Title;
