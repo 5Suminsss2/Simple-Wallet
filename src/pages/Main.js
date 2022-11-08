f8ac60c6559408ce4b3844e6deb28cd377db08ae
5Suminsss2 <sumin.kim.dev@gmail.com>
2022-11-08 15:37:10 +0900

    Design: Main 페이지 디자인

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,16 +3,33 @@ import Alaram from "../components/Alaram";
 import GraphAccount from "../components/GraphAccount";
 import NewAccountHistory from "../components/NewAccountHistory";
 import Title from "../components/Title";
+import styled from "styled-components";
 
 function Main () {
+
+    // CSS
+    const Wrapper = styled.section`
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 380px;
+      margin: 30px auto;
+      padding: 20px;
+      background: #fff;
+      border-radius: 10px;
+      box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+        rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+    `;
+
     return (
-      <div>
+      <Wrapper>
         <Title />
         <Alaram />
         <GraphAccount />
         <AccountHistory />
         <NewAccountHistory />
-      </div>
+      </Wrapper>
     );
 }
 
