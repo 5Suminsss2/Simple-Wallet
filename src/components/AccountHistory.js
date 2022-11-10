48ad04826e5f50371753f5b3e2e9b34af6e6b578
5Suminsss2 <sumin.kim.dev@gmail.com>
2022-11-10 16:37:44 +0900

    Feat: 거래내역 데이터를 json 데이터로 화면에 뿌리기

--- src/components/AccountHistory.js
+++ src/components/AccountHistory.js
@@ -1,6 +1,14 @@
 import styled from "styled-components";
+import accountHistoryData from "../data/accountHistoryData.json";
+import AccountHistoryCard from "./AccountHistoryCard";
 
 function AccountHistory() {
+
+  const accountHistoryCard = (data) =>{
+    return(
+      <AccountHistoryCard data={data} />
+    )
+  }
 
   // CSS
   const AccountHistoryContainer = styled.section`
@@ -15,49 +23,11 @@ function AccountHistory() {
     margin-bottom: 10px;
   `
 
-  const AccountHistoryDetail = styled.div`
-    /* display: flex;
-    justify-content: space-between;
-    align-items: center; */
-    height: 55px;
-    margin-bottom: 10px;
-    padding: 0 10px;
-    font-size: 14px;
-    background-color: #fff;
-    border-radius: 10px;
-    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
-      rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
-  `;
-
-  const AccountHistoryDate = styled.div`
-    padding: 5px 0;
-    font-size: 11px;
-  `;
-
-  const AccountHistoryContents = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  `;
-
   return (
     <AccountHistoryContainer>
       <AccountHistoryTitle>입출금 내역</AccountHistoryTitle>
       <div>
-        <AccountHistoryDetail>
-          <AccountHistoryDate>2022-10-11</AccountHistoryDate>
-          <AccountHistoryContents>
-            <div>교촌치킨 허니콤보</div>
-            <div>- 21,000 원</div>
-          </AccountHistoryContents>
-        </AccountHistoryDetail>
-        <AccountHistoryDetail>
-          <AccountHistoryDate>2022-10-10</AccountHistoryDate>
-          <AccountHistoryContents>
-            <div>알바비</div>
-            <div>+ 100,000 원</div>
-          </AccountHistoryContents>
-        </AccountHistoryDetail>
+        {accountHistoryData["accountHistoryData"].map(accountHistoryCard)}
       </div>
     </AccountHistoryContainer>
   );
---
+++ src/components/AccountHistoryCard.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+function AccountHistoryCard({data}) {
+
+    // 화폐 3자리마다 콤마 넣기
+    const price = data.price.toLocaleString("ko-KR");
+    
+    // CSS
+    const AccountHistoryDetail = styled.div`
+        height: 55px;
+        margin-bottom: 10px;
+        padding: 0 10px;
+        font-size: 14px;
+        background-color: #fff;
+        border-radius: 10px;
+        box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+        rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+    `;
+
+    const AccountHistoryDate = styled.div`
+        padding: 5px 0;
+        font-size: 11px;
+    `;
+
+    const AccountHistoryContents = styled.div`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    `;
+
+    return (
+        <AccountHistoryDetail>
+        <AccountHistoryDate>{data.date}</AccountHistoryDate>
+        <AccountHistoryContents>
+            <div>{data.accountContents}</div>
+            {data.accountType === "Deposit" ? (
+            <div>+ {price} 원</div>
+            ) : (
+            <div>- {price} 원</div>
+            )}
+        </AccountHistoryContents>
+        </AccountHistoryDetail>
+    );
+}
+
+export default AccountHistoryCard;
