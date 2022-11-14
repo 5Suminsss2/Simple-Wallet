4325d268b9e209ec81a0b929f4fe296822ecfd83
5Suminsss2 <sumin.kim.dev@gmail.com>
2022-11-14 15:50:42 +0900

    Feat: 입출금 내역 계산 기능 구현

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,68 +1,86 @@
 import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { datasetState } from "../store/atom";
 
 function Total() {
 
-    const TotalContainer = styled.section`
-        margin: 15px 0;
-    `
-    const TotalItem = styled.div`
-        width: 310px;
-        height: 130px;
-        margin-top: 20px;
-        padding: 5px 30px;
-        border-radius: 10px;
-        background-color: #d9d9d9;
-    `;
+  // CSS
+  const TotalContainer = styled.section`
+    margin: 15px 0;
+  `;
+  const TotalItem = styled.div`
+    width: 310px;
+    height: 130px;
+    margin-top: 20px;
+    padding: 5px 30px;
+    border-radius: 10px;
+    background-color: #d9d9d9;
+  `;
 
-    const TotalTitle = styled.div`
-        margin-top: 10px;
-        font-size: 20px;
-    ` 
-    
-    const TotalAccount = styled.div`
-      margin-top: 15px;
-      font-size: 32px;
-    `;
+  const TotalTitle = styled.div`
+    margin-top: 10px;
+    font-size: 20px;
+  `;
 
-    const SeperateTotalContainer = styled.div`
-      display: flex;
-      justify-content: space-between;
-      margin: 30px 0;
-      
-    `;
+  const TotalAccount = styled.div`
+    margin-top: 15px;
+    font-size: 32px;
+  `;
 
-    const SeperateTotalItems = styled.div`
-      width: 42%;
-      height: 60px;
-      padding: 12px;
-      border-radius: 10px;
-      background-color: #d9d9d9;
-    `;
+  const SeperateTotalContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 30px 0;
+  `;
 
-    const SeperateTotalTitle = styled.div`
-      font-size: 14px;
-      margin-bottom: 6px;
-    `;
+  const SeperateTotalItems = styled.div`
+    width: 42%;
+    height: 60px;
+    padding: 12px;
+    border-radius: 10px;
+    background-color: #d9d9d9;
+  `;
 
-    const SeperateTotalAccount = styled.div`
-      font-size: 18px;
-      margin-left: 22px;
-    `;
+  const SeperateTotalTitle = styled.div`
+    font-size: 14px;
+    margin-bottom: 6px;
+  `;
+
+  const SeperateTotalAccount = styled.div`
+    font-size: 18px;
+    margin-left: 22px;
+  `;
+
+  // 입출금 내역 데이터 가져오기
+  const dataset = useRecoilValue(datasetState);
+  let deposit = 0;
+  let withDraw = 0;
+
+  // 입출금 내역 데이터 계산
+  dataset.map(function (element) {
+    if (element.accountType === "Deposit") {
+      deposit += element.price
+    } else {
+      withDraw += element.price
+    }
+  });
+
+  let total = (deposit - withDraw).toLocaleString("ko-KR");
 
   return (
     <TotalContainer>
       <TotalItem>
         <TotalTitle>Total</TotalTitle>
-        <TotalAccount>300,000 원</TotalAccount>
+        <TotalAccount>{total} 원</TotalAccount>
       </TotalItem>
       <SeperateTotalContainer>
         <SeperateTotalItems>
           <SeperateTotalTitle>총 지출</SeperateTotalTitle>
-          <SeperateTotalAccount>- 300,000 원</SeperateTotalAccount>
+          <SeperateTotalAccount>- {withDraw} 원</SeperateTotalAccount>
         </SeperateTotalItems>
         <SeperateTotalItems>
           <SeperateTotalTitle>총 입금</SeperateTotalTitle>
-          <SeperateTotalAccount>+ 600,000 원</SeperateTotalAccount>
+          <SeperateTotalAccount>+ {deposit} 원</SeperateTotalAccount>
         </SeperateTotalItems>
       </SeperateTotalContainer>
     </TotalContainer>
